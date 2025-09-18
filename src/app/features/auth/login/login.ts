6e06cff9ec2d622624299293c3a8732a85373471
Henry Frungilo <henryfrungilo@outlook.com>
2025-09-18 09:31:27 -0300

    feat: implementar componente de login com form básico

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [InputTextModule, PasswordModule, ButtonModule, CommonModule, FormsModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Handle login logic here
+    }
+  }
+
+
 
 }
